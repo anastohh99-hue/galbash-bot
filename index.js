9e284a49a565306f86585507b03e713c24d71dc8
anastohh99-hue <anastohh99@gmail.com>
2026-05-29 04:37:36 -0900

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('guildMemberAdd', async member => {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         // 🌟 المقاسات الجديدة بناءً على رسمتك (أكبر، ويمين في النص) 🌟
-        const avatarSize = 175; // كبرنا الحجم بشكل واضح
+        const avatarSize = 155; // كبرنا الحجم بشكل واضح
         const avatarX = 70;    // سحبناها لليمين في المكان الفاضي اللي حددته
         const avatarY = 165;    // رفعناها شوي عشان تتناسق مع الحجم الجديد
 
