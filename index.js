469b840523f7bb5ac5c96a362f2347dccabfdfd2
anastohh99-hue <anastohh99@gmail.com>
2026-05-29 06:03:56 -0900

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('guildMemberAdd', async member => {
         // 🌟 المقاسات عشان تلاحظ الفرق بعينك (يسار أكثر) 🌟
         const avatarSize = 200; 
         const avatarX = 500;    // دفيناها يسار بزيادة عشان يبين الفرق
-        const avatarY = 250;    
+        const avatarY = 100;    
 
         // قص الدائرة
         const radius = avatarSize / 2;
