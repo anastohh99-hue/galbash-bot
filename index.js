7546c4a5ae13c1b4870cd17c44581cf2d3cc0fad
anastohh99-hue <anastohh99@gmail.com>
2026-05-29 06:06:57 -0900

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('guildMemberAdd', async member => {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         // 🌟 المقاسات عشان تلاحظ الفرق بعينك (يسار أكثر) 🌟
-        const avatarSize = 120; 
+        const avatarSize = 130; 
         const avatarX = 260;    // دفيناها يسار بزيادة عشان يبين الفرق
         const avatarY = 100;    
 
